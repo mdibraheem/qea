99e677ccf9f572fc4c7cd264eead088447fe46af
Scott Plybon <sjplybon@gmail.com>
2016-03-21 12:12:40 -0400

    Events model controller routes working

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,32 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#
+Event.create([
+  {
+    name: 'International Boat Show',
+    active: true,
+    starts_on: 5.days.ago,
+    ends_on: 10.days.from_now
+  },
+  {
+    name: 'Kentucky Derby',
+    active: true,
+    starts_on: '1 Oct 2015',
+    ends_on:  '8 May 2016',
+  },
+  {
+    name: 'Probowl',
+    active: false,
+    starts_on: '10 Aug 2016',
+    ends_on:  '31 Jan 2017',
+  },
+  {
+    name: 'UFC',
+    active: true,
+    starts_on: '5 Apr 2016',
+    ends_on:  '21 Sep 2016',
+  }
+  ])
+
 #   Mayor.create(name: 'Emanuel', city: cities.first)
